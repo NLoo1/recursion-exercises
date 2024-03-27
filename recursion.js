2b884f48d71b46210cda35c766a7af1a13e53f66
Nicholas L <NLoo1@outlook.com>
2024-03-27 09:56:22 -0400

    Add all functions

--- recursion.js
+++ recursion.js
@@ -1,50 +1,70 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-
+  if(nums.length == 0) return 1
+  return nums[0] * product(nums.slice(1))
 }
 
-/** longest: return the length of the longest word in an array of words. */
+// /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if(words.length == 0) return 0 
+  return Math.max(words[0].length, longest(words.slice(1)))
 }
 
-/** everyOther: return a string with every other letter. */
+// /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
-
+  if(str.length == 0) return ""
+  return str[0] + everyOther(str.slice(2))
 }
 
-/** isPalindrome: checks whether a string is a palindrome or not. */
+// /** isPalindrome: checks whether a string is a palindrome or not. */
 
+// racecar
 function isPalindrome(str) {
-
+  if(str.length==0 || str.length ==1) return true
+  if (str[0] == str[str.length-1]) return isPalindrome(str.slice(1,-1))
+  else return false
 }
 
-/** findIndex: return the index of val in arr (or -1 if val is not present). */
+// /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
-
+  if(arr.length == 0) return -1
+  if(arr[0] == val) return 0
+  else{
+    const result = findIndex(arr.slice(1), val);
+    if (result === -1) return -1
+    else return 1 + result
+  }
 }
 
-/** revString: return a copy of a string, but in reverse. */
+// /** revString: return a copy of a string, but in reverse. */
 
+// abc
 function revString(str) {
-
+  if(str.length == 0) return ""
+  return revString(str.slice(1)) + str[0]
 }
 
-/** gatherStrings: given an object, return an array of all of the string values. */
+// /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
+  let stringValues = [];
 
-}
+  function traverseObject(obj) {
+      for (let key in obj) {
+          if (typeof obj[key] === 'string') {
+              stringValues.push(obj[key]);
+          } else if (typeof obj[key] === 'object') {
+              traverseObject(obj[key]);
+          }
+      }
+  }
 
-/** binarySearch: given a sorted array of numbers, and a value,
- * return the index of that value (or -1 if val is not present). */
-
-function binarySearch(arr, val) {
-
+  traverseObject(obj);
+  return stringValues;
 }
 
 module.exports = {
@@ -54,6 +74,5 @@ module.exports = {
   isPalindrome,
   findIndex,
   revString,
-  gatherStrings,
-  binarySearch
+  gatherStrings
 };
--- recursion.test.js
+++ recursion.test.js
@@ -5,8 +5,7 @@ const {
   isPalindrome,
   findIndex,
   revString,
-  gatherStrings,
-  binarySearch
+  gatherStrings
 } = require("./recursion");
 
 describe("product", function() {
@@ -42,7 +41,7 @@ describe("isPalindrome", function() {
     expect(isPalindrome("racecar")).toBe(true);
     expect(isPalindrome("a")).toBe(true);
     expect(isPalindrome("helloolleh")).toBe(true);
-  });
+  })
 
   it("returns false if the string is not a palindrome", function() {
     expect(isPalindrome("tacodog")).toBe(false);
@@ -110,15 +109,4 @@ describe("gatherStrings", function() {
   });
 });
 
-describe("binarySearch", function () {
-  it("should find the index of a value in a sorted array", function() {
-    expect(binarySearch([1, 2, 3, 4], 4)).toEqual(3);
-    expect(binarySearch([1, 2], 1)).toEqual(0);
-    expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(5);
-  });
 
-  it("should return -1 if the value is not found", function() {
-    expect(binarySearch([1, 2, 3, 4], 0)).toEqual(-1);
-    expect(binarySearch([1, 2], 11)).toEqual(-1);
-  });
-});
